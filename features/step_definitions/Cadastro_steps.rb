2205c9fd851ba3b09b8738007612c0cbf7e3e155
Ingrid Bezerra <ingridnaval2014@gmail.com>
2021-10-28 11:37:55 -0300

    CenariosE2E

---
+++ features/step_definitions/Cadastro_steps.rb
@@ -0,0 +1,18 @@
+
+
+
+Dado('que preencho os campos validos') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando('todos os campos forem preenchidos') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando('validados') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então('deverei receber um e-mail de confirmação de cadastro') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
