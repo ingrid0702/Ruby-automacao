7cb8621de3631d2436b4a157640b690a05b8f071
Ingrid Bezerra <ingridnaval2014@gmail.com>
2021-11-09 14:29:31 -0300

    Teste E2E 2.1

--- features/step_definitions/E2E_steps.rb
+++ features/step_definitions/E2E_steps.rb
@@ -1,96 +1,43 @@
+Dado("que acessa a pagina de produto") do
+  visit "https://magento.nublue.co.uk"
+  find(".products-grid.grid li:first-child").click
+  find(".swatch-attribute-options.clearfix .swatch-option.text:first-child").click
+  find(".swatch-attribute-options.clearfix .swatch-option.color:first-child").click
+end
 
+Quando("adicionar ao carrinho e finalizar o pedido") do
+  sleep 5
+  find("#product-addtocart-button").click
+  sleep 5
+  visit "https://magento.nublue.co.uk/checkout/cart/"
+  find('button[data-role="proceed-to-checkout"]').click
+end
 
+Quando("preenche os campos de dados pessoas com email {string} nome {string}") do |email, nome|
+  sleep 15
+  find("input[name='username']").set email
+  find("input[name='firstname']").set nome
+end
 
-Dado('que estou autenticado') do
-    pending # Write code here that turns the phrase above into concrete actions
+Quando("sobrenome  {string} empresa {string}") do |sobrenome, empresa|
+  find("input[name='lastname']").set sobrenome
+  find("input[name='company']").set empresa
 end
-  
-  Quando('estiver na pagina da minha conta') do
-    pending # Write code here that turns the phrase above into concrete actions
-end
-  
-  Quando('clico na logo') do
-    pending # Write code here that turns the phrase above into concrete actions
-end
-  
-  Então('deverei ser direcionado para a pagina de vitrine') do
-    pending # Write code here that turns the phrase above into concrete actions
-end
-  
-  Dado('que estou na pagina de vitrine') do
-    pending # Write code here that turns the phrase above into concrete actions
-end
-  
-  Quando('clicar no botão adicionar ao carrinho') do
-    pending # Write code here that turns the phrase above into concrete actions
-end
-  
-  Então('o produto devera ser exibido na pagina de carrinho') do
-    pending # Write code here that turns the phrase above into concrete actions
-end
-  
-  Dado('que estou na pagina de carrinho') do
-    pending # Write code here that turns the phrase above into concrete actions
-end
-  
-  Quando('selecionar os campos obrigatórios') do
-    pending # Write code here that turns the phrase above into concrete actions
-end
-  
-  Quando('clicar em adicionar ao carrinho') do
-    pending # Write code here that turns the phrase above into concrete actions
-end
-  
-  Então('o produto devera ser adicionado ao carrinho') do
-    pending # Write code here that turns the phrase above into concrete actions
-end
-  
-  Dado('que estou com um produto selecionado no carrinho') do
-    pending # Write code here that turns the phrase above into concrete actions
-end
-  
-  Quando('clicar no ícone do carrinho') do
-    pending # Write code here that turns the phrase above into concrete actions
-end
-  
-  Quando('selecionar finalizar pedido') do
-    pending # Write code here that turns the phrase above into concrete actions
-end
-  
-  Então('devera ser encaminhado para checkout') do
-    pending # Write code here that turns the phrase above into concrete actions
-end
-  
-  Dado('que estou na pagina checkout no stepe de envio') do
-    pending # Write code here that turns the phrase above into concrete actions
-end
-  
-  Quando('preencho os campos obrigatorios') do
-    pending # Write code here that turns the phrase above into concrete actions
 
+Quando("os dados de endereço que são rua {string}  bairro {string} Complemento {string}") do |rua, bairro, complemento|
+  find("input[name='street[0]']").set rua
+  find("input[name='street[1]']").set bairro
+  find("input[name='street[2]']").set complemento
+  find("select[name='country_id'] option[value='BR']").click
+  find("select[name='region_id'] option[value='489']").click
+  find('input[name="city"]').set('Recife')
+  find('input[name="postcode"]').set('51240-500')
+  find('input[name="telephone"]').set('81 9999-9999')
+  sleep 10
+  find('button[data-role="opc-continue"]').click
+  sleep 15
 end
-  
-  Quando('clico em próximo') do
-    pending # Write code here that turns the phrase above into concrete actions
+
+Então("a compra sera finalizada com sucesso") do
+  find('.payment-method._active button[type="submit"]').click
 end
-  
-  Então('devera ser encaminhado para o estepe de revisão e pagamento') do
-    pending # Write code here that turns the phrase above into concrete actions
-end
-  
-  Dado('que estou na pagina checkout no stepe de revisão e pagamento') do
-    pending # Write code here that turns the phrase above into concrete actions
-end
-  
-  Quando('seleciono a opção de boleto') do
-    pending # Write code here that turns the phrase above into concrete actions
-end
-  
-  Quando('clico em finalizar pedido') do
-    pending # Write code here that turns the phrase above into concrete actions
-end
-  
-  Então('deverá ser encaminhado para atela de sucesso') do
-    pending # Write code here that turns the phrase above into concrete actions
-end
-  
